6c1d1e2d8fa43625cdb25d6eb9236ef3859e958d
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2024-10-04 02:13:12 +0000

    corrections updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
 
 def pascal_triangle(n):
+    """
+    Return an empty list if n <= 0
+    """
     if n <= 0:
         return []
 
-    triangle = [[1]]  # Initialize the first row
+    """ 
+    Initialize the first row
+    """
+    triangle = [[1]]
 
+    """
+    Generate subsequent rows
+    """
     for i in range(1, n):
-        row = [1]  # Start each row with 1
-        # Generate the middle elements by summing the previous row elements
+        """ 
+        Start each row with 1
+        """
+        row = [1]
+        """ 
+        Calculate the middle elements
+        """
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)  # End each row with 1
+        row.append(1)  
+        """ 
+        End each row with 1
+        """
         triangle.append(row)
 
     return triangle
-
-# Testing the function using the provided `print_triangle` function
-if __name__ == "__main__":
-    def print_triangle(triangle):
-        for row in triangle:
-            print("[{}]".format(",".join([str(x) for x in row])))
-
-    print_triangle(pascal_triangle(5))
