be57313ca7d7903652ed8316717115415bfe0e02
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2024-11-20 05:31:17 +0000

    Updates on requirements

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,46 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
 const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
+  method: 'GET'
+};
 
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching the movie:', error);
-    return;
-  }
-
-  try {
-    const movieData = JSON.parse(body);
-    const characters = movieData.characters;
-
+request(options, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
     printCharacters(characters, 0);
-  } catch (err) {
-    console.error('Error parsing the response:', err);
   }
 });
 
-
-function printCharacters(characters, index) {
-  if (index >= characters.length) return;
-
-  request(characters[index], (error, response, body) => {
-    if (error) {
-      console.error('Error fetching character:', error);
-      return;
-    }
-
-    try {
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-      printCharacters(characters, index + 1);
-    } catch (err) {
-      console.error('Error parsing character data:', err);
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
     }
   });
 }
